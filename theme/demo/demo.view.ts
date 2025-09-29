56cbd6165ddf4234d4dec57fc4ae250038cc9d44
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-09-29 18:42:02 +0300

    theme

---
+++ theme/demo/demo.view.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+	export class $ainews_theme_demo extends $.$ainews_theme_demo {
+		hue_deg() {
+			return this.hue() + 'deg'
+		}
+
+		hue_spread_deg() {
+			return this.hue_spread() + 'deg'
+		}
+	}
+}
