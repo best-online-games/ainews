705d29abf3e5bd8422fc6cce927caf3207db794b
koplenov <koplenov@gmail.com>
2025-09-30 16:11:21 +0300

    added $mol_book2_catalog, separate settings

---
+++ app/settings/settings.view.ts
@@ -0,0 +1,26 @@
+namespace $.$$ {
+	export class $ainews_app_settings extends $.$ainews_app_settings {
+		
+		// tabs fields
+		Categories() {
+			return Object.keys($ainews_app_feed_links).map(category => this.Category_page(category))
+		}
+		category_title(category: any) {
+			return category
+		}
+		// sources fileds
+		suggestions(category: any)  {
+			return $ainews_app_feed_links[category as keyof typeof $ainews_app_feed_links]
+		}
+
+		// @$mol_mem_key
+		sources(id: string, next?: any){
+			console.log({id, next})
+
+			if(next !== undefined)
+				return $mol_state_local.value(id, next);
+			return $mol_state_local.value(id)
+		}
+		
+	}
+}
