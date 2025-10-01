819c2cc46d9d67b8f214b29fb46f2ebb519187fa
Yaroslav Silkin <ya.silkin@comitas.ru>
2025-10-01 23:12:15 +0300

    feat: add img for article

---
+++ app/feed/img/img.view.css.ts
@@ -0,0 +1,10 @@
+namespace $ {
+    const { rem } = $mol_style_unit
+
+    $mol_style_define($.$$.$ainews_app_feed_img, {
+        display: 'block',
+        objectFit: 'cover',
+        borderRadius: rem(0.5),
+        boxShadow: '0 2px 8px rgba(0,0,0,.08)',
+    })
+}
